c1a2facd6eb790ebff51653ca84a9d7e2a4947e5
Ratanak Pek <ratanakpek088@gmail.com>
2017-10-17 00:20:50 +0700

    udpate controller

--- Routes/authorRoutes.js
+++ Routes/authorRoutes.js
@@ -1,19 +1,70 @@
 var express=require("express");
 var routes=function (Author) {
     var authorRouter=express.Router();
-    authorRouter.route("/")
-        .post(function (req, res) {
-            var author=new Author(req.body);
-            author.save();
-            res.send(req.body);
 
+    authorRouter.use("/:authorId", function (req, res, next) {
+        if(req.params.authorId){
+            Author.findById(req.params.authorId, function (err, author) {
+                if(err)
+                    res.status(500).send(err);
+                else if(author){
+                    req.author=author;
+                    next();
+                }else{
+                    res.status(404).send("Not Found!");
+                }
+            });
+        }
+    });
+    authorRouter.route("/:authorId")
+        .delete(function(req, res) {
+            req.author.remove(req.params.authorId, function(err, author){
+                if(err)
+                    res.status(500).send(err);
+                else
+                    res.json(author);
+            })
         })
         .get(function(req, res){
-            Author.find(req.body.id, function(err, author){
-                if(err){
-                    req.status(500).send("Error");
-                }else{
-                    res.json(author);
+                    res.json(req.author);
+
+        })
+        .put(function(req, res){
+            req.author.name=req.body.name;
+            req.author.number=req.body.number;
+            req.author.book_type=req.body.book_type;
+            req.author.save();
+            res.json(req.author);
+        })
+        .patch(function(req, res){
+            if(req.body.name){
+
+                req.author.name=req.body.name;
+                req.author.save();
+                res.json(req.author);
+
+            }else{
+                res.status(404).send("Reqired name!");
+            }
+        });
+
+    authorRouter.route("/")
+        .post(function (req, res) {
+
+            if (req.body.name) {
+                var author = new Author(req.body);
+                author.save();
+                res.send(req.body);
+            }else{
+                res.status(404).send("Name not found!")
+            }
+    })
+        .get(function (req,res) {
+            Author.find(function(err, author){
+                if(err)
+                    res.status(500).send(err)
+                else{
+                    res.json(author)
                 }
             })
         });
