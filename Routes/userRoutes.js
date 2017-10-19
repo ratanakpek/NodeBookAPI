a0a2b472a375c520915da55c358b9912cb7f276d
Ratanak Pek <ratanakpek088@gmail.com>
2017-10-19 21:19:21 +0700

    check error in swagger

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -16,7 +16,7 @@ var routes=function(User, passport, data){
                     res.status(500).send(err);
                 else{
                     data.data=user;
-                    res.json(data)
+                    res.json(user)
                 }
 
             });
@@ -48,7 +48,7 @@ var routes=function(User, passport, data){
                 data : req.user
             }
 
-            res.send(data);
+            res.send(req.user);
         });
 
     userRotuer.route('/logouts')
@@ -60,7 +60,7 @@ var routes=function(User, passport, data){
                 data : req.user
             }
            // res.redirect('/');
-             res.send(data);
+             res.send(req.user);
         });
 
     return userRotuer;
